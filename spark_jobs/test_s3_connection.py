dba921da88fefb3d43282f48d3d72acf77417a97
Abimola Onibi <abimolaonibi@gmail.com>
2025-10-06 17:33:32 +0100

    Testing Spark

---
+++ spark_jobs/test_s3_connection.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+BUCKET = os.getenv("S3_BUCKET_NAME")
+
+spark = SparkSession.builder \
+    .appName("S3Test") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .getOrCreate()
+
+BUCKET = "your-bucket-name"
+print("🔍 Listing files in:", BUCKET)
+
+df = spark.read.text(f"s3a://{BUCKET}/raw/openaq/")  # should succeed if S3 + creds work
+print("✅ Successfully read S3 folder, row count:", df.count())
+
+spark.stop()

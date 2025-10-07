524fe055a5a9d7569ce5b9e579d261e3d9ce4854
Abimola Onibi <abimolaonibi@gmail.com>
2025-10-07 16:39:02 +0100

    Testing Spark

--- spark_jobs/test_s3_connection.py
+++ spark_jobs/test_s3_connection.py
@@ -5,11 +5,6 @@ spark = (
     SparkSession.builder
     .appName("S3BucketList")
     .master("spark://spark-master:7077")
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    .config("spark.hadoop.fs.s3a.path.style.access", "true")
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-    .config("spark.hadoop.fs.s3a.connection.timeout", "60000")
-    .config("spark.hadoop.fs.s3a.connection.establish.timeout", "60000")
     .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
     .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID"))
     .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY"))

0043f37204b735ce9d4b42dc18fa1f257686bf60
Abimola Onibi <abimolaonibi@gmail.com>
2025-10-07 12:28:32 +0100

    Testing Spark

--- spark_jobs/test_boto3_connection.py
+++ spark_jobs/test_boto3_connection.py
@@ -10,6 +10,3 @@ s3 = boto3.client(
 # list buckets
 for b in s3.list_buckets()["Buckets"]:
     print(b["Name"])
-
-# upload a local file
-s3.upload_file("/opt/spark/data/test.json", os.getenv("S3_BUCKET_NAME"), "raw/test.json")

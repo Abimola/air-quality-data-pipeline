57dee9f4d2b44f306bcd31b8e786ee6173728d74
Abimola Onibi <abimolaonibi@gmail.com>
2025-10-11 16:05:52 +0100

    Adding parameter category to dbt

--- dags/airquality_dbt/models/marts/dim_sensor.sql
+++ dags/airquality_dbt/models/marts/dim_sensor.sql
@@ -5,6 +5,7 @@ select distinct
     parameter_name,
     display_name,
     units,
-    parameter_category
+    parameter_category,
+    sensor_status
 from {{ ref('base_air_quality') }}
 where sensor_id is not null
